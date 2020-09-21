069aad20743270ad6a050e192de5c0c418246979
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-09-21 15:31:18 -0600

    Packages ya con clases
    Listos para trabajar

---
+++ API/tramites_aeropuerto/src/main/java/org/una/tramites_aeropuerto/controllers/NotificacionesController.java
@@ -0,0 +1,15 @@
+package org.una.tramites_aeropuerto.controllers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author andre
+ */
+public class NotificacionesController {
+    
+}
