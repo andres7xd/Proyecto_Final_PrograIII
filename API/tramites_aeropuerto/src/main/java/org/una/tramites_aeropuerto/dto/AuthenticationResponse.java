36577b0b69f9a5deb5ab77b01362d935892c7872
RacheVC <rachecordero12@gmail.com>
2020-09-24 09:23:51 -0600

    clases de autenticacion response y request
    clases de autenticacion response y request

---
+++ API/tramites_aeropuerto/src/main/java/org/una/tramites_aeropuerto/dto/AuthenticationResponse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tramites_aeropuerto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author rache
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AuthenticationResponse {
+
+    private String jwt;
+    private UsuariosDTO usuario;
+    //private List<PermisoOtorgadoDTO> permisos;
+}
--- API/tramites_aeropuerto/src/main/java/org/una/tramites_aeropuerto/services/UsuariosServiceImplementation.java
+++ API/tramites_aeropuerto/src/main/java/org/una/tramites_aeropuerto/services/UsuariosServiceImplementation.java
@@ -8,8 +8,10 @@ package org.una.tramites_aeropuerto.services;
 import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.una.tramites_aeropuerto.dto.AuthenticationRequest;
 import org.una.tramites_aeropuerto.entities.Usuarios;
 import org.una.tramites_aeropuerto.repositories.IUsuariosRepository;
 
